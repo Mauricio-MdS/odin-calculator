556d6ca77989632a24de266265259e33b60f4c12
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-03-21 17:53:52 -0300

    feat: update the display with the operator buttons

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector(".equal");
 const display = document.querySelector("#result");
 
+const equation = [];
+const operators = ["+", "-", "*", "/"];
+
+let currentNumber = "";
 
 for (numberButton of numberButtons) {
-    numberButton.addEventListener("click", (event) => {
-        display.value += event.target.textContent
-    });
+    numberButton.addEventListener("click", (event) => numberButtonHandler(event.target));
 }
 
+for (operatorButton of operatorButtons) {
+    operatorButton.addEventListener("click", (event) => operatorButtonHandler(event.target));
+}
+
+equalButton.addEventListener("click", () => {
+    /*TODO*/
+})
+
+function numberButtonHandler(button) {
+        currentNumber += button.textContent
+        updateDisplay();
+}
+
+function operatorButtonHandler(button) {
+    if (!currentNumber) return;
+    equation.push(currentNumber);
+    equation.push(button.dataset.operation);
+    currentNumber = "";
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.value = "";
+    for (element of equation) {
+        display.value += element + " "
+    }
+    display.value += currentNumber;
+}
 
 
 function add(a , b) {
@@ -27,7 +59,7 @@ function subtract (a, b) {
     return a - b;
 }
 
-function operate(operator, a, b) {
+function operate(a, operator, b) {
     switch(operator) {
         case "+":
             return add(a, b);
