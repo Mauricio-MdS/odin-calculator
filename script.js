7517b91db0a54ad75cacdbc1eb13fa7a3616145d
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-03-21 16:12:56 -0300

    feat: add func that maps the operator to operation

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function multiply (a, b) {
 function subtract (a, b) {
     return a - b;
 }
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+":
+            return add(a, b);
+        case "/":
+            return divide(a, b);
+        case "*":
+            return multiply(a, b);
+        case "-":
+            return subtract(a, b);
+    }
+}
